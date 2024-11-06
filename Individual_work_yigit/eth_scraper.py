15b7ce31bba3e6f99c9a340e6b91db7aa9bdc53d
yigitalidemir <yigitalidemir7@gmail.com>
2024-11-06 16:18:21 +0100

    Finished eth_scraper.py and added csv file

--- Individual_work_yigit/eth_scraper.py
+++ Individual_work_yigit/eth_scraper.py
@@ -1,95 +1,139 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.actions.wheel_input import ScrollOrigin
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
+import pandas as pd
 import time
 
 # Set up the driver
-path = "/Users/yigitalidemir/Desktop/HSLU/Lectures/CIP/CIP_Project/Individual_work_yigit/chromedriver"
-service = Service(path)
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome()
 
 # ActionChain
 actions = ActionChains(driver)
 
-# Navigate to website
+
+# Part 1 Navigate to website
 driver.get("https://coincodex.com/")
 time.sleep(2)
 
-
 try:
-    '''
-    #Scroll down to the ethereum
-    btc = driver.find_element(By.XPATH, "//a[@href = '/crypto/bitcoin/']")
-    scroll_origin = ScrollOrigin.from_element(btc)
-    actions.scroll_from_origin(scroll_origin,0, 1000)
-    print("Scrolled down")
-    '''
     #Close the ad banner
     try:
-        close_banner = WebDriverWait(driver, 5).until(
+        close_banner = WebDriverWait(driver, 3).until(
             EC.element_to_be_clickable((By.CLASS_NAME, "close"))
         )
         close_banner.click()
-        print("Banner closed")
+        print("Banner closed.")
     except:
-        print("No banner")
+        print("No banner.")
 
     # Click on the Ethereum link
-    ethereum_link = WebDriverWait(driver, 5).until(
+    ethereum_link = WebDriverWait(driver, 3).until(
         EC.element_to_be_clickable((By.XPATH, "//a[@href = '/crypto/ethereum/']"))
     )
-    print("Ethereum link found")
+    print("Ethereum link found.")
     ethereum_link.click()
-    time.sleep(2)
+    time.sleep(1)
 
     #Click on the historical data
-    hist_data_button = WebDriverWait(driver, 5).until(
+    hist_data_button = WebDriverWait(driver, 3).until(
         EC.element_to_be_clickable((By.XPATH, "//a[@href = '/crypto/ethereum/historical-data/']"))
     )
-    print("Historical data button found")
+    print("Historical data button found.")
     hist_data_button.click()
-    time.sleep(2)
+    time.sleep(1)
 
-    #Arrange time period
+    # Find and click time period button
     time_period_button_css = "body > app-root > app-root > div > div > div > div > app-coin-history-data > section.seo-section.section-container > div > div.options-bar.d-flex.justify-content-between.align-items-center > div.date-select.button.button-secondary"
-    time_period_button = WebDriverWait(driver, 5).until(
+    time_period_button = WebDriverWait(driver, 3).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, time_period_button_css))
     )
-    print("Time period button found")
+    print("Time period button found.")
     time_period_button.click()
 
+    # Find field for start and en date
     start_date_button_css = "body > app-root > app-root > div > div > div > div > app-coin-history-data > section.seo-section.section-container > div > div.options-bar.d-flex.justify-content-between.align-items-center > div.date-select.button.button-secondary.dateToggle > div.date-picker > app-date-range-picker > div > div > div > div.calendars > input[type=date]:nth-child(1)"
     end_date_button_css = "body > app-root > app-root > div > div > div > div > app-coin-history-data > section.seo-section.section-container > div > div.options-bar.d-flex.justify-content-between.align-items-center > div.date-select.button.button-secondary.dateToggle > div.date-picker > app-date-range-picker > div > div > div > div.calendars > input[type=date]:nth-child(2)"
     select_button_css = "body > app-root > app-root > div > div > div > div > app-coin-history-data > section.seo-section.section-container > div > div.options-bar.d-flex.justify-content-between.align-items-center > div.date-select.button.button-secondary.dateToggle > div.date-picker > app-date-range-picker > div > div > div > div.dates-select > div.select > button"
-    start_date_button = WebDriverWait(driver, 5).until(
+    start_date_button = WebDriverWait(driver, 3).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, start_date_button_css))
     )
 
-    end_date_button = WebDriverWait(driver, 5).until(
+    end_date_button = WebDriverWait(driver, 3).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, end_date_button_css))
     )
 
-    select_button = WebDriverWait(driver, 5).until(
+    select_button = WebDriverWait(driver, 3).until(
         EC.element_to_be_clickable((By.CSS_SELECTOR, select_button_css))
     )
+    print("Start and End date button found.")
 
-    print("Start and End date button found")
-    start_date_button.send_keys("01012021")
+    # Send date input
+    start_date = "01012021"
+    end_date = "01012024"
+    start_date_button.send_keys(start_date)
     time.sleep(1)
-    end_date_button.send_keys("01012024")
+    end_date_button.send_keys(end_date)
     time.sleep(1)
     select_button.click()
-    time.sleep(3)
+    print("Given time-period selected.")
+    time.sleep(2)
 
+    # Part 2 BeautifulSoup Scrape
+    # Parse the page source
     soup = BeautifulSoup(driver.page_source, "html.parser")
-    table = soup.find("table", class_="formatted-table full-size-table").text
-    print(table)
+    table = soup.find("table", class_="formatted-table full-size-table")
+
+    # Extract headers
+    data = []
+    headers = [header.text for header in table.find_all("th")]
+    print("Scrape starting...")
+
+    # Iterate through 37 pages.
+    for i in range(36):
+        # Check if the first popup appears and close it if so
+        try:
+            first_popup_button = WebDriverWait(driver, 1).until(
+                EC.element_to_be_clickable((By.ID, "onesignal-slidedown-cancel-button"))
+            )
+            first_popup_button.click()
+            print("First popup closed.")
+            time.sleep(1)
+
+            # Check if the second popup appears and close it if so
+            try:
+                second_popup_button = WebDriverWait(driver, 1).until(
+                    EC.element_to_be_clickable((By.ID, "onesignal-slidedown-cancel-button"))
+                )
+                second_popup_button.click()
+                print("Second popup closed.")
+
+            except:
+                print("No second popup detected.")
+
+        except:
+            print("No popup detected.")
+
+        # Proceed with scraping the table data
+        rows = table.find_all("tr")
+        for row in rows[1:]:
+            cells = row.find_all("td")
+            data.append([cell.text.strip() for cell in cells])
+        print("Page", i+1, "completed.")
+
+        # Move to the page indicator and click to go to the next page
+        page_indicator = driver.find_element(By.XPATH, "//a[@aria-label = 'Next']")
+        actions.move_to_element(page_indicator).perform()
+        page_indicator.click()
+        time.sleep(1)
+
+    #Create dataframe
+    df = pd.DataFrame(data, columns=headers)
+    print(df)
+    df.to_csv("eth_scraped_data.csv", index=False)
+    print("Data scraped.")
 
 
 except Exception as e:
@@ -97,4 +141,4 @@ except Exception as e:
 
 finally:
     driver.quit()
-    print("Driver closed")
+    print("Driver closed.")
